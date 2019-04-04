662cf88d8914b47968727c422c935c9ba9f63082
chengzao <czhlink@163.com>
2019-04-04 22:47:55 +0800

    add other local mod package

--- helloMod/hello/hello.go
+++ helloMod/hello/hello.go
@@ -3,13 +3,17 @@ package main
 import (
 	"fmt"
 	"hello/utils"
+	"importMod/hello"
 
 	"github.com/jinzhu/configor"
 )
 
+// run : go run hello.go
 func main() {
 	fmt.Println("Hello Mod")
 	fmt.Println("使用外部包", configor.Config{})
 
 	fmt.Println("使用项目内包", utils.Hello("Hello"))
+
+	hello.Hello("引用另外一个本地的外部包")
 }
